f0434a10b1ab51c20b78a404355cb340ed404bfc
Parth Chavan <111859049+Parthchavan11@users.noreply.github.com>
2023-11-01 10:32:35 +0530

    Update IndexPage.js

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -14,8 +14,9 @@ export default function IndexPage() {
   
   return (
     <>
-    <section className="mx-14 mt-5 lg:mx-40">
-      <h1 className="mb-10 font-bold text-4xl ">THE BLOG</h1>
+    <section className="mx-14 mt-5  lg:mx-40">
+      <h1 className="mt-28 mb-8 text-black text-5xl text-center mt-10 font-bold animate-fade-in-down ">Exploring the World, One Blog at a Time</h1>
+      <h3 className="text-xl font-semibold text-center text-gray-400 ">Navigating Life's Adventures Through Words</h3>
     <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-3 ">
       {posts.length > 0 && posts.map(post => (
         <Post {...post} />
