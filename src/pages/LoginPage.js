6c3d5304b84754cbd3a193127b80a62de0b47b10
Parth Chavan <111859049+Parthchavan11@users.noreply.github.com>
2023-11-01 10:35:03 +0530

    Add files via upload

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -29,17 +29,20 @@ export default function LoginPage() {
     return <Navigate to={'/'} />
   }
   return (
-    <form className="login" onSubmit={login}>
-      <h1>Login</h1>
-      <input type="text"
-             placeholder="username"
+    <form className="login font-semibold text-3xl  text-gray-700 m-2 " onSubmit={login}>
+      <h1 classname="mt-6">Here you can Login </h1>
+      <h2 className="text-sm font-semibold text-black  text-center mb-3 mr-20"> Let's join us </h2>
+      <input type="text "
+      class="bg-gray-100 rounded-lg px-5 py-2 mb-4 text-center text-sm focus:border focus:outline-none text-black placeholder:text-gray-600 placeholder:opacity-50 font-small md:w-72 lg:w-[340px]"
+             placeholder="username "
              value={username}
              onChange={ev => setUsername(ev.target.value)}/>
       <input type="password"
+      class="bg-gray-100 rounded-lg px-5 py-2 mb-4 text-center text-sm focus:border focus:outline-none text-black placeholder:text-gray-600 placeholder:opacity-50 font-semibold md:w-72 lg:w-[340px]"
              placeholder="password"
              value={password}
              onChange={ev => setPassword(ev.target.value)}/>
-      <button>Login</button>
+      <button className="bg-purple-400 rounded-lg px-2 py-2 text-center text-sm focus:border focus:outline-none text-black placeholder:text-gray-600 placeholder:opacity-50 font-semibold md:w-72 lg:w-[340px]">Login</button>
     </form>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 module.exports = {
+  theme: {
+    extend: {
+        keyframes: {
+            'fade-in-down': {
+                '0%': {
+                    opacity: '0',
+                    transform: 'translateY(-20px)'
+                },
+                '100%': {
+                    opacity: '1',
+                    transform: 'translateY(0)'
+                },
+            }
+        },
+        animation: {
+            'fade-in-down': 'fade-in-down 1s ease-out'
+        }
+    },
+},
     content: ['./src/**/*.{js,jsx,ts,tsx}', './public/**/*.html'],
     // other configurations...
     plugins: [
      
       require('@tailwindcss/line-clamp'),
+      require("tailwindcss-animate")
     ],
+    
   };
   
